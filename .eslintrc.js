f662ca7bcd61f9763a89ea7548037043a4c08b5b
shangxinbo <314911714@qq.com>
2017-03-01 19:04:08 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'eslint:recommended',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    'no-implied-eval': 1,
+    'no-loop-func': 1,
+    'indent': 1,
+    'semi':["error", "never"],
+    'no-mixed-spaces-and-tabs': 1,
+    'no-console':0,
+    'no-unused-vars':0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
